73fc9fd11c5ac747ae320ef8c167294b72ee1627
Accessd <accessd0@gmail.com>
2017-09-20 13:07:17 +0300

    add support for redis sentinel

--- lib/anycable/pubsub.rb
+++ lib/anycable/pubsub.rb
@@ -9,7 +9,11 @@ module Anycable
     attr_reader :redis_conn
 
     def initialize
-      @redis_conn = Redis.new(url: Anycable.config.redis_url)
+      redis_config = { url: Anycable.config.redis_url }
+      unless Anycable.config.redis_sentinels.empty?
+        redis_config[:sentinels] = Anycable.config.redis_sentinels
+      end
+      @redis_conn = Redis.new(redis_config)
     end
 
     def broadcast(channel, payload)
--- spec/anycable/config_spec.rb
+++ spec/anycable/config_spec.rb
@@ -4,10 +4,18 @@ require 'spec_helper'
 
 describe Anycable::Config do
   subject(:config) { Anycable.config }
+  let(:sentinel_config) do
+    [
+      { 'host' => 'redis-1-1', 'port' => 26_379 },
+      { 'host' => 'redis-1-2', 'port' => 26_379 },
+      { 'host' => 'redis-1-3', 'port' => 26_379 }
+    ]
+  end
 
   it "loads config vars from anycable.yml", :aggregate_failures do
     expect(config.rpc_host).to eq "localhost:50123"
     expect(config.redis_url).to eq "redis://localhost:6379/2"
+    expect(config.redis_sentinels).to eq sentinel_config
     # default value
     expect(config.redis_channel).to eq "__anycable__"
     expect(config.log_level).to eq :info
