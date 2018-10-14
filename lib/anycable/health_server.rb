0896b84e4f2adb37dd0d05d18a21c76ee8e9a9fc
Vladimir Dementyev <dementiev.vm@gmail.com>
2018-10-14 16:37:47 -0400

    refactor: Anycable::Server
    - Use class instance instead of a module
    - Run gRPC server in a thread
    - Remove dependency on global config

--- lib/anycable/health_server.rb
+++ lib/anycable/health_server.rb
@@ -7,11 +7,13 @@ module Anycable
   # Server for HTTP healthchecks
   module HealthServer
     class << self
-      def start(port)
+      def start(grpc_server, port)
+        return unless Anycable.config.http_health_port_provided?
         return if running?
 
-        @health_server ||= build_server(port)
+        @health_server ||= build_server(grpc_server, port)
         Thread.new { @health_server.start }
+        at_exit { Anycable::HealthServer.stop }
 
         Anycable.logger.info "HTTP health server is listening on #{port}"
       end
@@ -31,14 +33,14 @@ module Anycable
       SUCCESS_RESPONSE = [200, "Ready"].freeze
       FAILURE_RESPONSE = [503, "Not Ready"].freeze
 
-      def build_server(port)
+      def build_server(grpc_server, port)
         WEBrick::HTTPServer.new(
           Port: port,
           Logger: Anycable.logger,
           AccessLog: []
         ).tap do |server|
           server.mount_proc '/health' do |_, res|
-            res.status, res.body = Anycable::Server.running? ? SUCCESS_RESPONSE : FAILURE_RESPONSE
+            res.status, res.body = grpc_server.running? ? SUCCESS_RESPONSE : FAILURE_RESPONSE
           end
         end
       end
--- spec/bg_helper.rb
+++ spec/bg_helper.rb
@@ -4,7 +4,3 @@ Thread.new do
   Anycable.config.http_health_port = 54_321
   Anycable::Server.start
 end
-
-at_exit do
-  Anycable::Server.stop
-end
