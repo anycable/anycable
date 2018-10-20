fd8fb04b512412cbdad5818b0701e5a44e229ae2
Vladimir Dementyev <dementiev.vm@gmail.com>
2018-10-20 19:42:08 -0400

    feature: add gRPC health check service
    Closes #29.
    
    Added standard gRPC health check service.
    See https://github.com/grpc/grpc/blob/master/doc/health-checking.md

---
+++ spec/integrations/grpc/health_check_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "health checker", :with_grpc_server do
+  before(:all) do
+    @service = Grpc::Health::Checker.rpc_stub_class
+                                    .new(Anycable.config.rpc_host, :this_channel_is_insecure)
+  end
+
+  let(:service) { @service }
+
+  let(:params) { {} }
+
+  subject { service.check(Grpc::Health::V1::HealthCheckRequest.new(params)) }
+
+  context "without service" do
+    specify do
+      expect { subject }.to raise_error(GRPC::NotFound)
+    end
+  end
+
+  context "with service" do
+    let(:params) { { service: "anycable.RPC" } }
+
+    specify do
+      expect(subject.status).to eq :SERVING
+    end
+  end
+end
