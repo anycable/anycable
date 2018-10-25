c2a1adfba8cbd5bea0a3b49e4fc95cd8a15c4dc2
Vladimir Dementyev <dementiev.vm@gmail.com>
2018-10-24 22:28:21 -0400

    add application booting to cli

--- spec/integrations/cli/options_spec.rb
+++ spec/integrations/cli/options_spec.rb
@@ -6,22 +6,32 @@ describe "CLI options", :cli do
   describe "version" do
     specify "-v" do
       run_cli("-v") do |cli|
-        expect(cli).to have_stopped
         expect(cli).to have_output_line("v#{Anycable::VERSION}")
+        expect(cli).to have_stopped
+        expect(cli).to have_exit_status(0)
       end
     end
 
     specify "--version" do
-      run_cli("-v") do |cli|
-        expect(cli).to have_stopped
+      run_cli("--version") do |cli|
         expect(cli).to have_output_line("v#{Anycable::VERSION}")
+        expect(cli).to have_stopped
+        expect(cli).to have_exit_status(0)
+      end
+    end
+
+    specify "--help" do
+      run_cli("--help") do |cli|
+        expect(cli).to have_output_line("$ anycable [options]")
+        expect(cli).to have_stopped
+        expect(cli).to have_exit_status(0)
       end
     end
   end
 
   describe "server options" do
     specify "--rpc-host" do
-      run_cli("--rpc-host localhost:50053") do |cli|
+      run_cli("--rpc-host localhost:50053 -r ../spec/support/dummy.rb") do |cli|
         expect(cli).to have_output_line("RPC server is listening on localhost:50053")
       end
     end
