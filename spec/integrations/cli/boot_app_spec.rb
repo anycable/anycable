b57405165e2c1b028f5f0b9ca761652e184a1aa2
Vladimir Dementyev <dementiev.vm@gmail.com>
2018-11-01 22:36:57 -0400

    upd app initialization order

--- spec/integrations/cli/boot_app_spec.rb
+++ spec/integrations/cli/boot_app_spec.rb
@@ -27,10 +27,9 @@ describe "CLI require app", :cli do
       FileUtils.rm_rf(File.join(PROJECT_ROOT, "bin/config"))
     end
 
-    it "loads application" do
+    it "loads Rails application" do
       run_cli do |cli|
-        expect(cli).to have_output_line("Loading application from ./config/environment.rb ...")
-        expect(cli).to have_output_line("Rails application is loaded")
+        expect(cli).to have_output_line("Serving Rails application from ./config/environment.rb")
       end
     end
   end
@@ -38,14 +37,12 @@ describe "CLI require app", :cli do
   context "when require option is present" do
     it "loads application when file exists" do
       run_cli("-r ../spec/support/dummy.rb") do |cli|
-        expect(cli).to have_output_line("Loading application from ../spec/support/dummy.rb ...")
-        expect(cli).to have_output_line("Application is loaded")
+        expect(cli).to have_output_line("Serving application from ../spec/support/dummy.rb")
       end
     end
 
     it "prints application when file couldn't be loaded" do
       run_cli("-r ../spec/support/dummy_fake.rb") do |cli|
-        expect(cli).to have_output_line("Loading application from ../spec/support/dummy_fake.rb ...")
         expect(cli).to have_output_line("cannot load such file")
         expect(cli).to have_stopped
         expect(cli).to have_exit_status(1)
