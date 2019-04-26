f2afac878b7949fc47b9f74e75c93972d293ce6c
Sergey Ponomarev <me@sergey-ponomarev.ru>
2019-04-26 17:12:54 -0400

    Add deprection warning for default listening interface

--- cli/cli.go
+++ cli/cli.go
@@ -20,6 +20,10 @@ var (
 	fs          *flag.FlagSet
 )
 
+const (
+	deprecatedHost = "0.0.0.0|deprecated"
+)
+
 func init() {
 	// Configure namespaced flagSet with errorHandling set to ExitOnError (=1)
 	fs = flag.NewFlagSetWithEnvPrefix(os.Args[0], "ANYCABLE", 1)
@@ -44,7 +48,7 @@ func init() {
 	}
 
 	// Config vars
-	fs.StringVar(&defaults.Host, "host", "0.0.0.0", "")
+	fs.StringVar(&defaults.Host, "host", deprecatedHost, "")
 	fs.IntVar(&defaults.Port, "port", portDefault, "")
 	fs.StringVar(&defaults.Path, "path", "/cable", "")
 	fs.StringVar(&defaults.HealthPath, "health-path", "/health", "")
@@ -105,7 +109,7 @@ USAGE
   anycable-go [options]
 
 OPTIONS
-  --host                   Server host, default: 0.0.0.0, env: ANYCABLE_HOST
+  --host                   Server host, default: 0.0.0.0 (deprecated, will be changed to "localhost"), env: ANYCABLE_HOST
   --port                   Server port, default: 8080, env: ANYCABLE_PORT, PORT
   --path                   WebSocket endpoint path, default: /cable, env: ANYCABLE_PATH
   --health-path            HTTP health endpoint path, default: /health, env: ANYCABLE_HEALTH_PATH
@@ -143,17 +147,27 @@ func PrintHelp() {
 }
 
 func prepareComplexDefaults() {
-		defaults.Headers = parseHeaders(headers)
+	if defaults.Host == deprecatedHost {
+		fmt.Println(
+			`DEPRECATION WARNING: You're using default host configuration which starts AnyCable-Go
+server on all available interfaces including external ones. This is about to be changed
+to loopback interface only in future versions. Please, consider switching to "localhost"
+or set "0.0.0.0" explicitly in your configuration, if you want to continue with
+the current behavior and supress this message.`)
 
-		if debugMode {
-			defaults.LogLevel = "debug"
-			defaults.LogFormat = "text"
-		}
-
-		if defaults.MetricsPort == 0 {
-			defaults.MetricsPort = defaults.Port
-		}
+		defaults.Host = "0.0.0.0"
 	}
+	defaults.Headers = parseHeaders(headers)
+
+	if debugMode {
+		defaults.LogLevel = "debug"
+		defaults.LogFormat = "text"
+	}
+
+	if defaults.MetricsPort == 0 {
+		defaults.MetricsPort = defaults.Port
+	}
+}
 
 // parseHeaders returns a headers list with the values from
 // a comma-separated string list
