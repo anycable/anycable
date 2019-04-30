aca829135f7817f6024ba912a93c0c2b36e43cac
Sergey Ponomarev <me@sergey-ponomarev.ru>
2019-04-30 16:22:47 -0400

    Cover utils.FetchHeaders with test

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -24,3 +24,26 @@ func TestFetchUID(t *testing.T) {
 		assert.Equal(t, "external-request-id", uid)
 	})
 }
+
+func TestFetchHeaders(t *testing.T) {
+	req := httptest.NewRequest("GET", "/", nil)
+	req.Header.Set("Cookies", "yummy_cookie=raisin; tasty_cookie=strawberry")
+	req.Header.Set("X-Api-Token", "42")
+	req.Header.Set("Accept-Language", "ru")
+
+	t.Run("Without specified headers ", func(t *testing.T) {
+		headers := FetchHeaders(req, []string{})
+
+		assert.Len(t, headers, 0)
+	})
+
+	t.Run("With specified headers ", func(t *testing.T) {
+		headers := FetchHeaders(req, []string{"cookies", "x-api-token"})
+
+		assert.Len(t, headers, 2)
+
+		assert.Equal(t, "42", headers["x-api-token"])
+		assert.Equal(t, "yummy_cookie=raisin; tasty_cookie=strawberry", headers["cookies"])
+		assert.Empty(t, headers["accept-language"])
+	})
+}
