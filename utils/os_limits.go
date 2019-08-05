57ce97140e3911bf0e3abe55b3424975e724d4c1
Vladimir Dementyev <dementiev.vm@gmail.com>
2019-08-05 18:56:25 -0400

    fix: handle unsupported syscall on windows

---
+++ utils/os_limits.go
@@ -0,0 +1,22 @@
+// +build darwin linux
+
+package utils
+
+import (
+	"fmt"
+	"syscall"
+)
+
+// OpenFileLimit returns a string displaying the current open file limit
+// for the process or unknown if it's not possible to detect it
+func OpenFileLimit() (limitStr string) {
+	var rLimit syscall.Rlimit
+
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
+		limitStr = "unknown"
+	} else {
+		limitStr = fmt.Sprintf("%d", rLimit.Cur)
+	}
+
+	return
+}
---
+++ utils/os_limits_win.go
@@ -0,0 +1,8 @@
+// +build windows
+
+package utils
+
+// OpenFileLimit is not supported on Windows
+func OpenFileLimit() string {
+	return "unsupported"
+}
