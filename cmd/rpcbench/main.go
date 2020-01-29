e893923343b7a6b78cc6c3530b924c5f1db54896
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-01-29 18:39:09 -0500

    chore: refactor rpcbench
    - Move pool vs mono bench to a separate file
    
    - Use rpc.Controller in bench

--- cmd/rpcbench/main.go
+++ cmd/rpcbench/main.go
@@ -1,62 +1,47 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
-	"math"
 	"os"
-	"sort"
-	"sync/atomic"
 	"time"
+
+	"github.com/anycable/anycable-go/metrics"
+	"github.com/anycable/anycable-go/stats"
+	nanoid "github.com/matoous/go-nanoid"
 
 	"github.com/golang-collections/go-datastructures/queue"
 
-	grpcpool "github.com/anycable/anycable-go/pool"
-	pb "github.com/anycable/anycable-go/protos"
+	"github.com/anycable/anycable-go/rpc"
 	"github.com/anycable/anycable-go/utils"
 	"github.com/apex/log"
 
 	"github.com/namsral/flag"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/keepalive"
-	"google.golang.org/grpc/status"
-)
-
-const (
-	retryExhaustedInterval   = 10
-	retryUnavailableInterval = 100
-	invokeTimeout            = 3000
 )
 
 // Options describes benchmark options
 type Options struct {
-	host            string
-	capacity        int
-	initialCapacity int
-	concurrency     int
-	total           int
-	pool            bool
-	debug           bool
+	host        string
+	capacity    int
+	concurrency int
+	total       int
+	channel     string
+	action      string
+	mode        string
+	debug       bool
 }
 
 // Benchmark implements shared methods for the benchmark
 type Benchmark struct {
+	rpc     *rpc.Controller
+	perform bool
+	channel string
+	action  string
+	metrics *metrics.Metrics
 	buffer  *queue.RingBuffer
-	pool    *grpcpool.Pool
-	sem     chan (struct{})
-	conn    *grpc.ClientConn
 	resChan chan (time.Duration)
 	errChan chan (error)
 }
-
-var kacp = keepalive.ClientParameters{
-	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
-	PermitWithoutStream: true,             // send pings even without active streams
-}
-
-var retryCount = uint64(0)
-var retryTime = uint64(0)
 
 func main() {
 	var b Benchmark
@@ -88,36 +73,27 @@ func main() {
 		"capacity",
 		options.capacity,
 	).WithField(
-		"pool",
-		options.pool,
+		"mode",
+		options.mode,
 	).Infof("Running RPC benchmark for %s", options.host)
 
-	factory := func() (*grpc.ClientConn, error) {
-		return grpc.Dial(options.host, grpc.WithInsecure(), grpc.WithKeepaliveParams(kacp))
-	}
+	channel, _ := json.Marshal(map[string]string{"channel": options.channel})
+	b.channel = string(channel)
+	action, _ := json.Marshal(map[string]string{"action": options.action})
+	b.action = string(action)
 
-	if options.pool {
-		p, err := grpcpool.NewChannelPool(options.initialCapacity, options.capacity, factory)
+	rpcConfig := rpc.NewConfig()
+	rpcConfig.Concurrency = options.capacity
+	rpcConfig.Host = options.host
 
-		if err != nil {
-			log.Errorf("!!! Failed to intialize RPC pool !!!\n%v", err)
-			os.Exit(1)
-		}
+	b.perform = options.mode == "perform"
+	b.metrics = metrics.NewMetrics(nil, 0)
+	b.rpc = rpc.NewController(b.metrics, &rpcConfig)
 
-		b.pool = &p
-	} else {
-		conn, err := grpc.Dial(options.host, grpc.WithInsecure(), grpc.WithKeepaliveParams(kacp), grpc.WithBalancerName("round_robin"))
+	err = b.rpc.Start()
 
-		if err != nil {
-			log.Errorf("!!! Failed to intialize RPC connection !!!\n%v", err)
-			os.Exit(1)
-		}
-
-		b.sem = make(chan struct{}, options.capacity)
-		for i := 0; i < options.capacity; i++ {
-			b.sem <- struct{}{}
-		}
-		b.conn = conn
+	if err != nil {
+		panic("Failed to initialize RPC")
 	}
 
 	b.resChan = make(chan time.Duration, 1000)
@@ -132,7 +108,7 @@ func main() {
 		go b.startWorker()
 	}
 
-	var resAgg resAggregate
+	var resAgg stats.ResAggregate
 
 	completed := 0
 	failures := 0
@@ -150,11 +126,9 @@ func main() {
 	}
 
 	printResults(&resAgg)
+	printMetrics(b.metrics)
 	if failures > 0 {
 		log.Errorf("%d requests failed out of %d", failures, completed)
-	}
-	if retryCount > 0 {
-		log.Warnf("%d requests retried (total time %dms)", retryCount, retryTime)
 	}
 }
 
@@ -162,14 +136,31 @@ func parseOptions(options *Options) {
 	flag.StringVar(&options.host, "u", "localhost:50051", "RPC server host and port")
 	flag.IntVar(&options.concurrency, "c", 10, "Number of concurrent requests")
 	flag.IntVar(&options.capacity, "cap", 30, "RPC clients pool capacity (max size)")
-	flag.IntVar(&options.initialCapacity, "init", 10, "RPC clients pool initial capacity")
 	flag.IntVar(&options.total, "t", 1000, "Total number of requests to perform")
-	flag.BoolVar(&options.pool, "pool", false, "Use clients pool or single client")
+	flag.StringVar(&options.mode, "mode", "perform", "Choose benchmark mode: connect or perform")
+	flag.StringVar(&options.channel, "channel", "BenchmarkChannel", "Channel to subscribe to")
+	flag.StringVar(&options.action, "action", "echo", "Action to perform")
 	flag.BoolVar(&options.debug, "debug", false, "Enable debug logging")
 	flag.Parse()
 }
 
 func (b *Benchmark) startWorker() {
+	path := "/cable"
+	headers := &map[string]string{"cookie": "test_id=rpc-bench"}
+	uuid, err := nanoid.Nanoid()
+
+	if err != nil {
+		panic("Nanoid failed")
+	}
+
+	// perform authenticate to warmup "session"
+	ids, _, err := b.rpc.Authenticate(uuid, path, headers)
+
+	if err != nil {
+		log.Errorf("Failed to authenticate: %v", err)
+		return
+	}
+
 	for {
 		_, err := b.buffer.Get()
 
@@ -178,10 +169,11 @@ func (b *Benchmark) startWorker() {
 		}
 
 		start := time.Now()
-		if b.pool != nil {
-			err = b.performPoolRequest()
+
+		if b.perform {
+			_, err = b.rpc.Perform(uuid, ids, b.channel, b.action)
 		} else {
-			err = b.performRequest()
+			_, _, err = b.rpc.Authenticate(uuid, path, headers)
 		}
 
 		if err != nil {
@@ -192,159 +184,19 @@ func (b *Benchmark) startWorker() {
 	}
 }
 
-func (b *Benchmark) performPoolRequest() (err error) {
-	conn, err := (*b.pool).Get()
-
-	if err != nil {
-		return
-	}
-
-	defer conn.Close()
-
-	client := pb.NewRPCClient(conn.Conn)
-
-	_, err = retry(func() (interface{}, error) {
-		return client.Connect(context.Background(), &pb.ConnectionRequest{Path: "/cable", Headers: make(map[string]string)})
-	})
-
-	return
-}
-
-func (b *Benchmark) performRequest() (err error) {
-	<-b.sem
-	defer func() { b.sem <- struct{}{} }()
-
-	client := pb.NewRPCClient(b.conn)
-
-	_, err = retry(func() (interface{}, error) {
-		return client.Connect(context.Background(), &pb.ConnectionRequest{Path: "/cable", Headers: make(map[string]string)})
-	})
-
-	return
-}
-
-func retry(callback func() (interface{}, error)) (res interface{}, err error) {
-	retryAge := 0
-	attempt := 0
-	wasExhausted := false
-
-	for {
-		res, err = callback()
-
-		if err == nil {
-			return res, nil
-		}
-
-		if retryAge > invokeTimeout {
-			return nil, err
-		}
-
-		st, ok := status.FromError(err)
-		if !ok {
-			return nil, err
-		}
-
-		log.Debugf("RPC failure %v %v", st.Message(), st.Code())
-
-		interval := retryUnavailableInterval
-
-		if st.Code() == codes.ResourceExhausted {
-			interval = retryExhaustedInterval
-			if !wasExhausted {
-				attempt = 0
-				wasExhausted = true
-			}
-		} else if wasExhausted {
-			wasExhausted = false
-			attempt = 0
-		}
-
-		delayMS := int(math.Pow(2, float64(attempt))) * interval
-		delay := time.Duration(delayMS)
-
-		retryAge += delayMS
-
-		atomic.AddUint64(&retryCount, 1)
-		atomic.AddUint64(&retryTime, uint64(delayMS))
-
-		time.Sleep(delay * time.Millisecond)
-
-		attempt++
-	}
-}
-
-func printResults(res *resAggregate) {
+func printResults(res *stats.ResAggregate) {
 	log.Infof("95p=%dms    min=%dms    median=%dms    max=%dms",
-		roundToMS(res.Percentile(95)),
-		roundToMS(res.Min()),
-		roundToMS(res.Percentile(50)),
-		roundToMS(res.Max()),
+		stats.RoundToMS(res.Percentile(95)),
+		stats.RoundToMS(res.Min()),
+		stats.RoundToMS(res.Percentile(50)),
+		stats.RoundToMS(res.Max()),
 	)
 }
 
-func roundToMS(d time.Duration) int64 {
-	return int64((d + (500 * time.Microsecond)) / time.Millisecond)
-}
+func printMetrics(m *metrics.Metrics) {
+	stats := m.IntervalSnapshot()
 
-// From https://github.com/anycable/websocket-bench/blob/master/benchmark/stat.go
-type resAggregate struct {
-	samples []time.Duration
-	sorted  bool
-}
-
-type byAsc []time.Duration
-
-func (a byAsc) Len() int           { return len(a) }
-func (a byAsc) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a byAsc) Less(i, j int) bool { return a[i] < a[j] }
-
-func (agg *resAggregate) Add(rtt time.Duration) {
-	agg.samples = append(agg.samples, rtt)
-	agg.sorted = false
-}
-
-func (agg *resAggregate) Count() int {
-	return len(agg.samples)
-}
-
-func (agg *resAggregate) Min() time.Duration {
-	if agg.Count() == 0 {
-		return 0
+	if stats["rpc_retries_total"] > 0 {
+		log.Warnf("%d retries were made", stats["rpc_retries_total"])
 	}
-	agg.Sort()
-	return agg.samples[0]
-}
-
-func (agg *resAggregate) Max() time.Duration {
-	if agg.Count() == 0 {
-		return 0
-	}
-	agg.Sort()
-	return agg.samples[len(agg.samples)-1]
-}
-
-func (agg *resAggregate) Percentile(p int) time.Duration {
-	if p <= 0 {
-		panic("p must be greater than 0")
-	} else if 100 <= p {
-		panic("p must be less 100")
-	}
-
-	agg.Sort()
-
-	rank := p * len(agg.samples) / 100
-
-	if agg.Count() == 0 {
-		return 0
-	}
-
-	return agg.samples[rank]
-}
-
-func (agg *resAggregate) Sort() {
-	if agg.sorted {
-		return
-	}
-	sort.Sort(byAsc(agg.samples))
-	agg.sorted = true
 }
---
+++ stats/stats.go
@@ -0,0 +1,81 @@
+// Package stats contains calculation utils for benchmarks
+// Based on https://github.com/anycable/websocket-bench/blob/master/benchmark/stat.go
+package stats
+
+import (
+	"sort"
+	"time"
+)
+
+// RoundToMS returns the number of milliseconds for the given duration
+func RoundToMS(d time.Duration) int64 {
+	return int64((d + (500 * time.Microsecond)) / time.Millisecond)
+}
+
+// ResAggregate contains duration samples
+type ResAggregate struct {
+	samples []time.Duration
+	sorted  bool
+}
+
+type byAsc []time.Duration
+
+func (a byAsc) Len() int           { return len(a) }
+func (a byAsc) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byAsc) Less(i, j int) bool { return a[i] < a[j] }
+
+// Add adds a new sample to the aggregate
+func (agg *ResAggregate) Add(rtt time.Duration) {
+	agg.samples = append(agg.samples, rtt)
+	agg.sorted = false
+}
+
+// Count returns the number of samples
+func (agg *ResAggregate) Count() int {
+	return len(agg.samples)
+}
+
+// Min returns the min value
+func (agg *ResAggregate) Min() time.Duration {
+	if agg.Count() == 0 {
+		return 0
+	}
+	agg.sort()
+	return agg.samples[0]
+}
+
+// Max returns the max value
+func (agg *ResAggregate) Max() time.Duration {
+	if agg.Count() == 0 {
+		return 0
+	}
+	agg.sort()
+	return agg.samples[len(agg.samples)-1]
+}
+
+// Percentile returns the p-th precentile
+func (agg *ResAggregate) Percentile(p int) time.Duration {
+	if p <= 0 {
+		panic("p must be greater than 0")
+	} else if 100 <= p {
+		panic("p must be less 100")
+	}
+
+	agg.sort()
+
+	rank := p * len(agg.samples) / 100
+
+	if agg.Count() == 0 {
+		return 0
+	}
+
+	return agg.samples[rank]
+}
+
+func (agg *ResAggregate) sort() {
+	if agg.sorted {
+		return
+	}
+	sort.Sort(byAsc(agg.samples))
+	agg.sorted = true
+}
