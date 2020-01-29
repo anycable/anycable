fc299b84a2a90c910e5d3883d9944339275aff26
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-01-28 21:53:11 -0500

    feat(rpc): upgrade schema; add request support to channels

--- Makefile
+++ Makefile
@@ -3,3 +3,5 @@ all: build
 build:
 	grpc_tools_ruby_protoc -I ./protos --ruby_out=./lib/anycable/rpc --grpc_out=./lib/anycable/rpc ./protos/rpc.proto
 	sed -i '' '/'rpc_pb'/d' ./lib/anycable/rpc/rpc_services_pb.rb
+	sed -i '' 's/Anycable/AnyCable/g' ./lib/anycable/rpc/*_pb.rb
+	bundle exec rubocop -a ./lib/anycable/rpc
--- lib/anycable/rpc/rpc_pb.rb
+++ lib/anycable/rpc/rpc_pb.rb
@@ -6,48 +6,54 @@
 require "google/protobuf"
 
 Google::Protobuf::DescriptorPool.generated_pool.build do
-  add_message "anycable.ConnectionRequest" do
-    optional :path, :string, 1
-    map :headers, :string, :string, 2
-  end
-  add_message "anycable.ConnectionResponse" do
-    optional :status, :enum, 1, "anycable.Status"
-    optional :identifiers, :string, 2
-    repeated :transmissions, :string, 3
-    optional :error_msg, :string, 4
-  end
-  add_message "anycable.CommandMessage" do
-    optional :command, :string, 1
-    optional :identifier, :string, 2
-    optional :connection_identifiers, :string, 3
-    optional :data, :string, 4
-  end
-  add_message "anycable.CommandResponse" do
-    optional :status, :enum, 1, "anycable.Status"
-    optional :disconnect, :bool, 2
-    optional :stop_streams, :bool, 3
-    repeated :streams, :string, 4
-    repeated :transmissions, :string, 5
-    optional :error_msg, :string, 6
-  end
-  add_message "anycable.DisconnectRequest" do
-    optional :identifiers, :string, 1
-    repeated :subscriptions, :string, 2
-    optional :path, :string, 3
-    map :headers, :string, :string, 4
-  end
-  add_message "anycable.DisconnectResponse" do
-    optional :status, :enum, 1, "anycable.Status"
-    optional :error_msg, :string, 2
-  end
-  add_enum "anycable.Status" do
-    value :ERROR, 0
-    value :SUCCESS, 1
-    value :FAILURE, 2
+  add_file("rpc.proto", syntax: :proto3) do
+    add_message "anycable.Env" do
+      optional :path, :string, 1
+      map :headers, :string, :string, 2
+    end
+    add_message "anycable.ConnectionRequest" do
+      optional :env, :message, 3, "anycable.Env"
+    end
+    add_message "anycable.ConnectionResponse" do
+      optional :status, :enum, 1, "anycable.Status"
+      optional :identifiers, :string, 2
+      repeated :transmissions, :string, 3
+      optional :error_msg, :string, 4
+    end
+    add_message "anycable.CommandMessage" do
+      optional :command, :string, 1
+      optional :identifier, :string, 2
+      optional :connection_identifiers, :string, 3
+      optional :data, :string, 4
+      optional :env, :message, 5, "anycable.Env"
+    end
+    add_message "anycable.CommandResponse" do
+      optional :status, :enum, 1, "anycable.Status"
+      optional :disconnect, :bool, 2
+      optional :stop_streams, :bool, 3
+      repeated :streams, :string, 4
+      repeated :transmissions, :string, 5
+      optional :error_msg, :string, 6
+    end
+    add_message "anycable.DisconnectRequest" do
+      optional :identifiers, :string, 1
+      repeated :subscriptions, :string, 2
+      optional :env, :message, 5, "anycable.Env"
+    end
+    add_message "anycable.DisconnectResponse" do
+      optional :status, :enum, 1, "anycable.Status"
+      optional :error_msg, :string, 2
+    end
+    add_enum "anycable.Status" do
+      value :ERROR, 0
+      value :SUCCESS, 1
+      value :FAILURE, 2
+    end
   end
 end
 
 module AnyCable
+  Env = Google::Protobuf::DescriptorPool.generated_pool.lookup("anycable.Env").msgclass
   ConnectionRequest = Google::Protobuf::DescriptorPool.generated_pool.lookup("anycable.ConnectionRequest").msgclass
   ConnectionResponse = Google::Protobuf::DescriptorPool.generated_pool.lookup("anycable.ConnectionResponse").msgclass
   CommandMessage = Google::Protobuf::DescriptorPool.generated_pool.lookup("anycable.CommandMessage").msgclass
