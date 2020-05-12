73b9945619244749f8aa22169bde99eac83e63cc
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-05-12 17:53:49 +0300

    fix: inherit from ServerIntercepter
    It provides required callbacks stubs. Otherwise health checking doesn't work

--- lib/anycable/middleware.rb
+++ lib/anycable/middleware.rb
@@ -5,7 +5,7 @@ require "grpc"
 module AnyCable
   # Middleware is a wrapper over gRPC interceptors
   # for request/response calls
-  class Middleware < GRPC::Interceptor
+  class Middleware < GRPC::ServerInterceptor
     def request_response(request: nil, call: nil, method: nil)
       # Call middlewares only for AnyCable service
       return yield unless method.receiver.is_a?(AnyCable::RPCHandler)
@@ -13,6 +13,11 @@ module AnyCable
       call(request, call, method) do
         yield
       end
+    end
+
+    def server_streamer(**kwargs)
+      p kwargs
+      yield
     end
 
     def call(*)
