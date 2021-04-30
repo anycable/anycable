1302a10b220341d1d13efc45bf917df3b6414515
Vladimir Dementyev <dementiev.vm@gmail.com>
2021-04-30 14:20:56 +0300

    feat: CLI.embed!

--- spec/dummies/embedded.rb
+++ spec/dummies/embedded.rb
@@ -8,11 +8,8 @@ require_relative "../support/test_factory"
 
 AnyCable.connection_factory = AnyCable::TestFactory
 
-cli = AnyCable::CLI.new(embedded: true)
-cli.run(["--version-check-enabled=false"])
+AnyCable::CLI.embed!(["--version-check-enabled=false"])
 
 p "Server started"
-
-at_exit { cli.shutdown }
 
 loop {}
