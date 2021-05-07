95dc943ebff9e84019d425cde0a63e0243aa0dcc
Vladimir Dementyev <dementiev.vm@gmail.com>
2021-05-07 13:14:42 +0000

    chore: add pprof block and mutex profilers to diagnostics

--- diagnostics/diagnostics_gops.go
+++ diagnostics/diagnostics_gops.go
@@ -3,6 +3,13 @@
 package diagnostics
 
 import (
+	"fmt"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"runtime"
+	"strconv"
+
 	log "github.com/apex/log"
 	"github.com/google/gops/agent"
 )
@@ -10,5 +17,37 @@ import (
 func init() {
 	if err := agent.Listen(agent.Options{}); err != nil {
 		log.Fatal(err.Error())
+	}
+
+	pprofRequired := false
+
+	if vals := os.Getenv("BLOCK_PROFILE_RATE"); vals != "" {
+		val, err := strconv.Atoi(vals)
+
+		if err != nil {
+			log.Fatalf("Invalid value for block profile rate: %s", vals)
+		}
+
+		runtime.SetBlockProfileRate(val)
+		pprofRequired = true
+		fmt.Println("[PPROF] Block profiling enabled")
+	}
+
+	if vals := os.Getenv("MUTEX_PROFILE_FRACTION"); vals != "" {
+		val, err := strconv.Atoi(vals)
+
+		if err != nil {
+			log.Fatalf("Invalid value for mutex profile fraction: %s", vals)
+		}
+
+		runtime.SetMutexProfileFraction(val)
+		pprofRequired = true
+		fmt.Println("[PPROF] Mutex profiling enabled")
+	}
+
+	// Run pprof web server as well to be able to capture blocks and mutex profiles
+	// (not supported by gops)
+	if pprofRequired {
+		go func() { http.ListenAndServe(":6060", nil) }()
 	}
 }
