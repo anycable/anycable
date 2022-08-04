6b98d2cf605b8a89d421485ddf74a81fd6fc9844
Vladimir Dementyev <dementiev.vm@gmail.com>
2022-08-03 22:34:08 -0400

    chore: add generic ToJSON function

--- utils/utils.go
+++ utils/utils.go
@@ -1,12 +1,22 @@
 package utils
 
 import (
+	"encoding/json"
 	"os"
 
+	"github.com/apex/log"
 	"github.com/mattn/go-isatty"
 )
 
 // IsTTY returns true if program is running with TTY
 func IsTTY() bool {
 	return isatty.IsTerminal(os.Stdout.Fd())
+}
+
+func ToJSON[T any](val T) []byte {
+	jsonStr, err := json.Marshal(&val)
+	if err != nil {
+		log.Fatalf("😲 Failed to build JSON for %v: %v", val, err)
+	}
+	return jsonStr
 }
